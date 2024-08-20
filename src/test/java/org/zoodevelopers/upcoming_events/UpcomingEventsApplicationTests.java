b4f4be4efd4ee287b4f0f8925cb6a0b7059525a9
Hugo Perez Santos <hugopsantos@hotmail.com>
2024-08-20 12:59:32 +0200

    first commit

---
+++ src/test/java/org/zoodevelopers/upcoming_events/UpcomingEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zoodevelopers.upcoming_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcomingEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
