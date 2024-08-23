65fc97ebd6209a646b9fb8bbc1384cae39fd2696
Nacho Rodríguez Martín <nachorm9@hotmail.com>
2024-08-23 14:47:54 +0200

    register service

--- src/main/java/org/zoodevelopers/upcoming_events/controllers/RegisterController.java
+++ src/main/java/org/zoodevelopers/upcoming_events/controllers/RegisterController.java
@@ -1,5 +1,11 @@
 package org.zoodevelopers.upcoming_events.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zoodevelopers.upcoming_events.dtos.UserDto;
+
 @RestController
 @RequestMapping(path = "${api-endpoint}/register")
 public class RegisterController {
