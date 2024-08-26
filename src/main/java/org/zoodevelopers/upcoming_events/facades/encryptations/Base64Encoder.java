7a4459ea1ed3ee9e91f7fcb1ff70497f3c83193e
Gisela Soledad Hurtado <hurtadogiselasoledad@gmail.com>
2024-08-26 11:36:18 +0200

    Base 64 Encoder done

--- src/main/java/org/zoodevelopers/upcoming_events/facades/encryptations/Base64Encoder.java
+++ src/main/java/org/zoodevelopers/upcoming_events/facades/encryptations/Base64Encoder.java
@@ -1,5 +1,21 @@
 package org.zoodevelopers.upcoming_events.facades.encryptations;
 
-public class Base64Encoder {
-    
+import java.util.Base64;
+
+import org.zoodevelopers.upcoming_events.implementations.events.IEncoder;
+
+public class Base64Encoder implements IEncoder {
+
+    @Override
+    public String encode(String data) {
+        String dataEncoded = Base64.getEncoder().encodeToString(data.getBytes());
+        return dataEncoded;
+    }
+
+    public String decode(String data) {
+        byte[] decodedBytes = Base64.getDecoder().decode(data);
+        String dataDecoded = new String(decodedBytes);
+        return dataDecoded;
+    }
+
 }
