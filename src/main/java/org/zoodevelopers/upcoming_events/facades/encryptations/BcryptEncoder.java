b10f53400fffae76d48acfdb9de6c5a171abbfd8
Gisela Soledad Hurtado <hurtadogiselasoledad@gmail.com>
2024-08-26 11:37:23 +0200

    Bcrypt Encoder done

--- src/main/java/org/zoodevelopers/upcoming_events/facades/encryptations/BcryptEncoder.java
+++ src/main/java/org/zoodevelopers/upcoming_events/facades/encryptations/BcryptEncoder.java
@@ -1,5 +1,20 @@
 package org.zoodevelopers.upcoming_events.facades.encryptations;
 
-public class BcryptEncoder {
-    
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.zoodevelopers.upcoming_events.implementations.events.IEncoder;
+
+public class BcryptEncoder implements IEncoder {
+
+    BCryptPasswordEncoder encoder;
+
+    public BcryptEncoder(BCryptPasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    @Override
+    public String encode(String data) {
+        String dataEncoded = encoder.encode(data);
+        return dataEncoded;
+    }
+
 }
